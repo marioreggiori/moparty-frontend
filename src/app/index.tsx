e1b38f2ea2937ec3569362704094e24eef285e9e
Mario Reggiori <git@marioreggiori.com>
2020-10-09 23:02:19 +0200

    add auto refresh to remaining playlists

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,31 +4,45 @@ import Explorer from './explorer';
 import Playlists from './playlists';
 import NowPlaying from './now_playing';
 import { useSetRecoilState } from 'recoil';
-import { upNextListState, currentSongState } from './state';
+import { upNextListState, currentSongState, playListState, wishListState } from './state';
 
 export default (props: any) => {
   const setUpNext = useSetRecoilState(upNextListState);
   const setCurrentSong = useSetRecoilState(currentSongState);
+  const setWishList = useSetRecoilState(wishListState);
+  const setPlayList = useSetRecoilState(playListState);
 
   useEffect(() => {
     setInterval(async () => {
+
       try {
         await fetch('/up_next')
           .then(res => res.json())
           .then(data => setUpNext(data.tracks));
-      } catch (e) {
-
-      }
+      } catch (e) { }
 
       try {
         await fetch('/track/now_playing')
           .then(res => res.json())
           .then(data => setCurrentSong(data));
-      } catch (e) {
+      } catch (e) { }
 
-      }
-    }, 5000);
-  }, [setUpNext, setCurrentSong]);
+      try {
+        await fetch('/user_wishlist/my')
+          .then(res => res.json())
+          .then(data => setWishList(data.tracks));
+      } catch (e) { }
+
+      try {
+        await fetch('/user_playlist/my')
+          .then(res => res.json())
+          .then(data => setPlayList(data.tracks));
+      } catch (e) { }
+
+    }, 10000);
+
+
+  }, [setUpNext, setCurrentSong, setWishList, setPlayList]);
 
   return (
     <div id="app">
